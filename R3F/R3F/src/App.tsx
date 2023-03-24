502ecd8d792a7b60157eb81e1d6e8ad31ed8f3f6
Teon Mehta <teon.mehta@hl.co.uk>
2023-03-24 14:23:27 +0000

    42. OrbitControls

---
+++ R3F/R3F/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import './App.css';
+import Experience from './components/Experience';
+
+function App() {
+	return (
+		<Canvas>
+			<Experience />
+		</Canvas>
+	);
+}
+
+export default App;
---
+++ R3F/R3F/src/components/Experience.tsx
@@ -0,0 +1,42 @@
+import { useRef } from 'react';
+
+import { useThree, extend, useFrame } from '@react-three/fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+extend({ OrbitControls });
+
+export default function Experience() {
+	const three = useThree();
+	const cubeRef = useRef();
+	const groupRef = useRef();
+	useFrame((state, delta) => {
+		cubeRef.current.rotation.x += delta;
+		//groupRef.current.rotation.y += delta;
+	});
+
+	return (
+		<>
+			{/* <OrbitControls /> */}
+			<group ref={groupRef}>
+				<mesh position-x={-2}>
+					<sphereGeometry />
+					<meshBasicMaterial color='orange' />
+				</mesh>
+				<mesh
+					ref={cubeRef}
+					rotation={[0, Math.PI * 0.25, 0]}
+					position-x={2}
+					scale={1.5}
+				>
+					<boxGeometry args={[1.4, 1, 1]} />
+					<meshBasicMaterial args={[{ color: 'blue' }]} />
+				</mesh>
+			</group>
+
+			<mesh rotation-x={-Math.PI * 0.45} position-y={-1} scale={10}>
+				<planeGeometry />
+				<meshBasicMaterial args={[{ color: 'green' }]} />
+			</mesh>
+		</>
+	);
+}
